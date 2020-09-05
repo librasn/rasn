1507617a214af6207561c6362124dfdb3a1c0633
Erin Power <xampprocky@gmail.com>
2020-09-05 23:13:16 +0200

    more docs

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -17,6 +17,11 @@ fn __print_stream(stream: proc_macro2::TokenStream) -> proc_macro::TokenStream {
     stream.into()
 }
 
+
+/// An automatic derive of the `Decode` trait.
+///
+/// Will automatically generate a decode implementation using the your
+/// container's definition.
 #[proc_macro_derive(Decode, attributes(rasn))]
 pub fn decode_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
@@ -32,6 +37,10 @@ pub fn decode_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
     .into()
 }
 
+/// An automatic derive of the `Encode` trait.
+///
+/// Will automatically generate a encode implementation using the your
+/// container's definition.
 #[proc_macro_derive(Encode, attributes(rasn))]
 pub fn encode_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // Parse the input tokens into a syntax tree
@@ -55,10 +64,15 @@ pub fn encode_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
 /// and generate a *compile-time* check that all of your fields (if struct) or
 /// variants (if a choice style enum) have distinct tags.
 ///
-/// **Shared Attributes**
+/// ##### Shared Attributes
 /// These attributes are available on containers, variants, and fields.
-/// - *`tag([class], number)` — override the default tag with the one
-/// specified 
+/// - *`tag([class], number)`* — override the default tag with the one
+/// specified with this attribute. E.g. `#[rasn(rag(context, 0))]`.
+///
+/// ##### Container Attributes
+/// - `crate_root` The path to the `rasn` library to use in the macro.
+/// - `enumerated/choice` Use either `#[rasn(choice)]` or `#[rasn(enumerated)]`
+/// to mark your enum as one of the two different enum types in ASN.1.
 #[proc_macro_derive(AsnType, attributes(rasn))]
 pub fn asn_type_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // Parse the input tokens into a syntax tree
