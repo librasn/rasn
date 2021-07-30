33472149a2cb462e3b6edc208420e565525dbb0f
Kyle Schaffrick <kyle@raidi.us>
2021-07-30 15:38:48 -0500

    Emit type information when failing with CHOICE_ERROR_MESSAGE.
    This helps to make it easier to find the offending field in structs that
    have many choice fields by telling you what the type of the field is
    that's being incorrectly encoded/decoded.

--- macros/src/encode.rs
+++ macros/src/encode.rs
@@ -92,7 +92,11 @@ pub fn derive_enum_impl(
             encoder.encode_enumerated(tag, *self as isize).map(drop)
         }
     } else {
-        let error = crate::CHOICE_ERROR_MESSAGE;
+        let error = format!(
+            "Encoding field of type `{}`: {}",
+            name.to_string(),
+            crate::CHOICE_ERROR_MESSAGE
+        );
         quote! {
             Err::<(), _>(#crate_root::enc::Error::custom(#error))
         }
