002836ceb64d0e7b33b034f81e67035127b01659
Erin Power <xampprocky@gmail.com>
2021-09-17 11:50:25 +0200

    Add public methods for Any

--- src/types/any.rs
+++ src/types/any.rs
@@ -1,6 +1,31 @@
+use alloc::vec::Vec;
+
 /// Represents a complete encoded ASN.1 value of any type. Usually identified
 /// with an [`ObjectIdentifier`][crate::types::ObjectIdentifier].
 #[derive(Clone, Debug, PartialOrd, Ord, PartialEq, Eq)]
 pub struct Any {
-    pub(crate) contents: alloc::vec::Vec<u8>,
+    pub(crate) contents: Vec<u8>,
+}
+
+impl Any {
+    /// Creates a new wrapper around the opaque value.
+    pub fn new(contents: Vec<u8>) -> Self {
+        Self { contents }
+    }
+
+    /// Provides the raw representation of the value as bytes.
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.contents
+    }
+
+    /// Converts `Self` into the raw representation of the value.
+    pub fn into_bytes(self) -> Vec<u8> {
+        self.contents
+    }
+}
+
+impl AsRef<[u8]> for Any {
+    fn as_ref(&self) -> &[u8] {
+        self.contents.as_ref()
+    }
 }
