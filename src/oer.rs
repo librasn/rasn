067adc710be243f87c0e6d3d569b76a8272e23c0
Niklas Saari <niklas.saari@tutanota.com>
2023-08-12 18:47:40 +0300

    oer: improved integer testing, bug fixes

--- src/oer.rs
+++ src/oer.rs
@@ -52,8 +52,27 @@ mod tests {
         round_trip!(oer, bool, false, &[0]);
     }
     #[test]
-    fn integer() {
+    fn integer_no_constraints() {
+        // Without constraints, all integers should be encoded as signed, with length determinant,
+        // and without padding.
+        round_trip!(oer, Integer, 0.into(), &[0x01u8, 0x00]);
+        round_trip!(oer, Integer, 1.into(), &[0x01u8, 0x01]);
+        round_trip!(oer, Integer, (-1).into(), &[0x01u8, 0xff]);
         round_trip!(oer, Integer, 255.into(), &[0x02u8, 0x00, 0xff]);
         round_trip!(oer, Integer, (-255).into(), &[0x02u8, 0xff, 0x01]);
+        round_trip!(oer, Integer, i16::MAX.into(), &[0x02u8, 0x7f, 0xff]);
+        round_trip!(oer, Integer, i16::MIN.into(), &[0x02u8, 0x80, 0x00]);
+        round_trip!(
+            oer,
+            Integer,
+            (i32::from(i16::MAX) + 1).into(),
+            &[0x03u8, 0x00u8, 0x80, 0x00]
+        );
+        round_trip!(
+            oer,
+            Integer,
+            (i32::from(i16::MIN) - 1).into(),
+            &[0x03u8, 0xff, 0x7f, 0xff]
+        );
     }
 }
