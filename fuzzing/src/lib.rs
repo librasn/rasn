be8005323fae34bac56bcb5c2c1e551b25fb9123
Niklas Saari <niklas.saari@tutanota.com>
2023-12-04 18:58:57 +0200

    fuzzing improvements

--- fuzzing/src/lib.rs
+++ fuzzing/src/lib.rs
@@ -1,20 +1,28 @@
 // Attempts to decode random fuzz data and if we're successful, we check
 // that the encoder can produce encoding that the is *semantically*
 // equal to the original decoded value. So we decode that value back
-// into Rust because the encoder is guarenteed to produce the same
+// into Rust because the encoder is guaranteed to produce the same
 // encoding as the accepted input since `data` could contain trailing
 // bytes not used by the decoder.
+use rasn::prelude::*;
+
 macro_rules! fuzz_type {
-    ($data:expr, $($typ:ty),+ $(,)?) => {
+    ($codec:ident, $data:expr, $($typ:ty),+ $(,)?) => {
         $(
-            if let Ok(value) = rasn::der::decode::<$typ>($data) {
-                assert_eq!(value, rasn::der::decode(&rasn::der::encode(&value).unwrap()).unwrap());
+            if let Ok(value) = rasn::$codec::decode::<$typ>($data) {
+                assert_eq!(value, rasn::$codec::decode(&rasn::$codec::encode(&value).unwrap()).unwrap());
             }
         )+
     }
 }
 
-pub fn fuzz(data: &[u8]) {
-    fuzz_type!(data, rasn_pkix::Certificate);
+pub fn fuzz_oer(data: &[u8]) {
+    fuzz_type!(oer, data, Integer);
+}
+pub fn fuzz_pkix(data: &[u8]) {
+    fuzz_type!(der, data, rasn_pkix::Certificate);
 }
 
+pub fn fuzz(_data: &[u8]) {
+    todo!()
+}
--- fuzzing/src/main.rs
+++ fuzzing/src/main.rs
@@ -1,8 +1,9 @@
-#[macro_use] extern crate afl;
+#[macro_use]
+extern crate afl;
 
 fn main() {
     afl::fuzz!(|data: &[u8]| {
-        fuzz::fuzz(data);
+        // fuzz::fuzz_pkix(data);
+        fuzz::fuzz_oer(data);
     });
 }
-
