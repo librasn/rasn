8ec568438f991307a7c1e5e39be214f11c6453d8
6d7a <56823591+6d7a@users.noreply.github.com>
2024-02-02 22:20:48 +0100

    fix(macro): recognize option references (#219)
    * fix(macro): recognize option references
    
    OptionalEnum::is_option_type did not properly process references to Options.
    References to Option types are present in enum values with named fields.
    
    * style: cargo fmt

---
+++ tests/issue217.rs
@@ -0,0 +1,20 @@
+use rasn::prelude::*;
+
+#[derive(Debug, Clone, AsnType, Encode, Decode)]
+#[rasn(choice)]
+enum Tx {
+    #[rasn(tag(explicit(0)))]
+    Test,
+    #[rasn(tag(explicit(1)))]
+    Reset {
+        #[rasn(tag(explicit(0)))]
+        keys: Option<Vec<u32>>,
+    },
+}
+
+#[test]
+fn issue_217() {
+    let tx = Tx::Reset { keys: None };
+    let encoded = rasn::der::encode(&tx).unwrap();
+    assert_eq!(encoded, vec![161, 2, 48, 0])
+}
