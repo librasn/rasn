33d22a67479182c96228259a92789190e0ec5c6e
6d7a <56823591+6d7a@users.noreply.github.com>
2024-02-13 00:06:38 +0100

    fix(types): stack overflow in ObjectIdentifier PartialEq (#223)

---
+++ tests/issue222.rs
@@ -0,0 +1,11 @@
+use rasn::types::ObjectIdentifier;
+
+#[test]
+fn issue222() {
+    let arr: &[u32] = &[1, 2, 3];
+    let oid = ObjectIdentifier::new(arr).unwrap();
+    if &oid != arr {
+        assert!(false);
+    }
+    println!("done");
+}
