96fea8646d29fa79603bf5f803d08694c4d97cfd
6d7a <westphal@consider-it.de>
2024-03-08 17:43:58 +0100

    test: add test for unit structs

--- tests/issue225.rs
+++ tests/issue225.rs
@@ -2,12 +2,21 @@ use rasn::prelude::*;
 
 #[derive(AsnType, Encode, Decode, Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash)]
 #[rasn(tag(application, 2))]
-#[rasn(delegate)]
-pub struct UnbindRequest(pub ());
+pub struct UnbindRequest;
 
 #[test]
 fn issue225() {
-    let unbind_request = UnbindRequest(());
+    let unbind_request = UnbindRequest;
     let encoded = rasn::ber::encode(&unbind_request).unwrap();
     assert_eq!(encoded, &[66, 0]);
+}
+
+#[derive(AsnType, Encode, Decode, Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash)]
+pub struct UntaggedUnit;
+
+#[test]
+fn untagged_unit_struct() {
+    let encoded_unit_struct = rasn::ber::encode(&UntaggedUnit).unwrap();
+    let encoded_unit = rasn::ber::encode(&()).unwrap();
+    assert_eq!(encoded_unit, encoded_unit_struct);
 }
