da2bda76516be8da94bd9583d6339af2b6e10f9b
Niklas Saari <niklas.saari@tutanota.com>
2024-03-21 12:28:57 +0200

    Revert fuzzing changes

--- fuzzing/src/bin/check_crashes.rs
+++ fuzzing/src/bin/check_crashes.rs
@@ -1,63 +1,15 @@
-use anyhow::{Context, Result};
-use clap::{ArgGroup, Parser};
-
-/// Run crash cases of fuzzing from a directory or a single file.
-#[derive(Parser, Debug)]
-#[clap(group(ArgGroup::new("input").required(true).args(&["crash_file", "crash_dir"])))]
-struct Cli {
-    /// The path fo single crash file, e.g. out/oer/default/crashes/crash-1
-    #[clap(long, value_parser)]
-    crash_file: Option<String>,
-    /// The crash directory, e.g. out/oer/default/crashes/
-    #[arg(long, value_parser)]
-    crash_dir: Option<String>,
-    /// The codec to run the crash file (oer, der) for now, in future requires target type as well
-    #[arg(long)]
-    codec: String,
-}
-
-fn main() -> Result<()> {
-    let args = Cli::parse();
-    let fuzz_fn = match &args.codec {
-        codec if codec == "oer" => fuzz::fuzz_oer,
-        codec if codec == "der" => fuzz::fuzz_pkix,
-        _ => fuzz::fuzz,
-    };
-    match (&args.crash_file, &args.crash_dir) {
-        (Some(file), None) => {
-            println!("Using file: {file}");
-            run_single_file(file.to_owned().into(), fuzz_fn);
-        }
-        (None, Some(dir)) => {
-            println!("Using directory: {dir}");
-            run_from_dir(dir.to_owned().into(), fuzz_fn)?;
-        }
-        _ => unreachable!(), // clap ensures one of them is provided
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    for file in std::fs::read_dir("out/default/crashes/")?
+        .filter_map(Result::ok)
+        .map(|entry| entry.path())
+    {
+        let case = file.file_stem().unwrap().to_str().unwrap().to_owned();
+        println!("Testing Crash case: `{}`", case);
+        fuzz::fuzz(&std::fs::read(file)?);
+        println!("Testing Crash case: `{}` successful", case);
     }
+
+    println!("No valid crash cases found.");
 
     Ok(())
-}
-fn run_from_dir(dir: std::path::PathBuf, fuzz_fn: fn(&[u8])) -> Result<()> {
-    let crash_dir = std::fs::read_dir(dir)
-        .with_context(|| "Could not find the crash directory.".to_string())?;
-
-    for file in crash_dir.filter_map(Result::ok).map(|entry| entry.path()) {
-        run_single_file(file, fuzz_fn);
-    }
-    Ok(())
-}
-
-fn run_single_file(file: std::path::PathBuf, fuzz_fn: fn(&[u8])) {
-    let case = file.file_stem().unwrap().to_str().unwrap().to_owned();
-    let result = std::panic::catch_unwind(|| {
-        fuzz_fn(&std::fs::read(file).unwrap());
-    });
-    match result {
-        Ok(_) => {
-            println!("Testing Crash case: `{case}` - successful without panics");
-        }
-        Err(err) => {
-            println!("Testing Crash case: `{case}` failed with panics: {err:?}");
-        }
-    }
 }
--- fuzzing/src/lib.rs
+++ fuzzing/src/lib.rs
@@ -1,34 +1,19 @@
 // Attempts to decode random fuzz data and if we're successful, we check
 // that the encoder can produce encoding that the is *semantically*
 // equal to the original decoded value. So we decode that value back
-// into Rust because the encoder is guaranteed to produce the same
+// into Rust because the encoder is guarenteed to produce the same
 // encoding as the accepted input since `data` could contain trailing
 // bytes not used by the decoder.
-use rasn::prelude::*;
-// On top of unconstrained `Integer`, we also test some constrained types.
-type IntegerA = ConstrainedInteger<0, { u64::MAX as i128 }>;
-type IntegerB = ConstrainedInteger<{ i64::MIN as i128 }, { i64::MAX as i128 }>;
-type IntegerC = ConstrainedInteger<{ i64::MIN as i128 }, 0>;
-
 macro_rules! fuzz_type {
-    ($codec:ident, $data:expr, $($typ:ty),+ $(,)?) => {
+    ($data:expr, $($typ:ty),+ $(,)?) => {
         $(
-            if let Ok(value) = rasn::$codec::decode::<$typ>($data) {
-                assert_eq!(value, rasn::$codec::decode::<$typ>(&rasn::$codec::encode(&value).unwrap()).unwrap());
+            if let Ok(value) = rasn::der::decode::<$typ>($data) {
+                assert_eq!(value, rasn::der::decode(&rasn::der::encode(&value).unwrap()).unwrap());
             }
         )+
     }
 }
 
-pub fn fuzz_oer(data: &[u8]) {
-    fuzz_type!(oer, data, IntegerA);
-    fuzz_type!(oer, data, IntegerB);
-    fuzz_type!(oer, data, IntegerC);
-}
-pub fn fuzz_pkix(data: &[u8]) {
-    fuzz_type!(der, data, rasn_pkix::Certificate);
-}
-
-pub fn fuzz(_data: &[u8]) {
-    todo!()
+pub fn fuzz(data: &[u8]) {
+    fuzz_type!(data, rasn_pkix::Certificate);
 }
