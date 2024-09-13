1ed8a493daf56b6bed389b97767e088439196597
Niklas Saari <niklas.saari@tutanota.com>
2024-09-14 02:10:57 +0300

    Also set works with shared buffer

--- src/oer/enc.rs
+++ src/oer/enc.rs
@@ -98,6 +98,8 @@ impl<'a> Encoder<'a> {
             output: Cow::Owned(RefCell::new(Vec::with_capacity(16))),
             set_output: <_>::default(),
             field_bitfield: <_>::default(),
+            // field_bitfield: LinearMap::<_, _, 5>::default(),
+            // field_bitfield: <_>::default(),
             current_field_index: <_>::default(),
             extension_fields: <_>::default(),
             is_extension_sequence: bool::default(),
@@ -155,30 +157,14 @@ impl<'a> Encoder<'a> {
             self.current_field_index += 1;
         }
     }
+    // Take data as param, same as vec.extend()
+
     fn extend(&mut self, tag: Tag) -> Result<(), EncodeError> {
         if self.options.set_encoding {
             // If not using mem::take here, remember to call output.clear() after encoding
             self.set_output
                 .insert(tag, core::mem::take(&mut self.output.borrow_mut()));
         }
-        // }
-        // _ => Err(EncodeError::length_exceeds_platform_size(self.codec()))?,
-        // }
-        // Ok(())
-        // } else {
-        //     // Err(EncodeError::output_buffer_not_set())
-        //     Err(EncodeError::length_exceeds_platform_size(self.codec()))?
-        // }
-        // match self.output.len().checked_add(bytes.len()) {
-        //     Some(_) => {
-        //         if self.options.set_encoding {
-        //             self.set_output.insert(tag, bytes);
-        //         } else {
-        //             self.output.extend(bytes);
-        //         }
-        //     }
-        //     _ => Err(EncodeError::length_exceeds_platform_size(self.codec()))?,
-        // }
         Ok(())
     }
     /// Encode a tag as specified in ITU-T X.696 8.7
