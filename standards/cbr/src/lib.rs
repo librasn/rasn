fa6f81b06bfb3a186f151c7dafb4404ee5c27006
Erin Power <mull_buoy.0t@icloud.com>
2024-10-19 20:17:18 +0200

    chore: fmt

--- standards/cbr/src/lib.rs
+++ standards/cbr/src/lib.rs
@@ -5,13 +5,13 @@ extern crate alloc;
 
 use rasn::prelude::*;
 
-pub const PAL_ZONE: &'static Oid = Oid::const_new(&[1,3,6,1,4,1,46609,1,2]);
-pub const PAL_FREQUENCY: &'static Oid = Oid::const_new(&[1,3,6,1,4,1,46609,1,3]);
-pub const CBSD_FCCID: &'static Oid = Oid::const_new(&[1,3,6,1,4,1,46609,1,4]);
-pub const CBSD_SERIAL: &'static Oid = Oid::const_new(&[1,3,6,1,4,1,46609,1,5]);
-pub const SAS_FRN: &'static Oid = Oid::const_new(&[1,3,6,1,4,1,46609,1,6]);
-pub const CPIR: &'static Oid = Oid::const_new(&[1,3,6,1,4,1,46609,1,7]);
-pub const TEST: &'static Oid = Oid::const_new(&[1,3,6,1,4,1,46609,1,8]);
+pub const PAL_ZONE: &'static Oid = Oid::const_new(&[1, 3, 6, 1, 4, 1, 46609, 1, 2]);
+pub const PAL_FREQUENCY: &'static Oid = Oid::const_new(&[1, 3, 6, 1, 4, 1, 46609, 1, 3]);
+pub const CBSD_FCCID: &'static Oid = Oid::const_new(&[1, 3, 6, 1, 4, 1, 46609, 1, 4]);
+pub const CBSD_SERIAL: &'static Oid = Oid::const_new(&[1, 3, 6, 1, 4, 1, 46609, 1, 5]);
+pub const SAS_FRN: &'static Oid = Oid::const_new(&[1, 3, 6, 1, 4, 1, 46609, 1, 6]);
+pub const CPIR: &'static Oid = Oid::const_new(&[1, 3, 6, 1, 4, 1, 46609, 1, 7]);
+pub const TEST: &'static Oid = Oid::const_new(&[1, 3, 6, 1, 4, 1, 46609, 1, 8]);
 
 #[derive(AsnType, Debug, Clone, Decode, Encode, PartialEq, Eq, Hash)]
 #[rasn(delegate)]
