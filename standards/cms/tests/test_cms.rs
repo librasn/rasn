ed72a00b98c8ddc6da9d72172d45805b9624a70d
Niklas Saari <niklas.saari@tutanota.com>
2024-10-21 15:39:45 +0300

    Remove CMS test prints

--- standards/cms/tests/test_cms.rs
+++ standards/cms/tests/test_cms.rs
@@ -16,7 +16,6 @@ fn test_cms_signed() {
     let info = decode::<ContentInfo>(SIGNED_DATA).unwrap();
     assert_eq!(CONTENT_SIGNED_DATA, info.content_type);
     let data = decode::<SignedData>(info.content.as_bytes()).unwrap();
-    // println!("{:#?}", data);
 
     assert_eq!(CONTENT_DATA, data.encap_content_info.content_type);
 
@@ -30,7 +29,6 @@ fn test_cms_encrypted() {
     let info = decode::<ContentInfo>(ENCRYPTED_DATA).unwrap();
     assert_eq!(CONTENT_ENVELOPED_DATA, info.content_type);
     let data = decode::<EnvelopedData>(info.content.as_bytes()).unwrap();
-    println!("{:#?}", data);
 
     assert_eq!(CONTENT_DATA, data.encrypted_content_info.content_type);
 
