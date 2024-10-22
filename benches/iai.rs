621b0acc124ab5da9b6ac7b80efbeb7d6b99375f
Sebastian Hahn <sebastian.h.hahn@mercedes-benz.com>
2024-10-22 06:54:06 +0200

    Update from iai to iai-callgrind
    This requires the iai-callgrind-runner binary to be installed (possible
    via cargo install), but has the advantage of working with a recent
    valgrind version (iai seems to be unmaintained for the past years).

--- benches/iai.rs
+++ benches/iai.rs
@@ -2,15 +2,20 @@ mod common;
 
 use common::*;
 
+use iai_callgrind::{main, library_benchmark_group, library_benchmark};
+use std::hint::black_box;
+
 macro_rules! encoding_rules {
     ($($encoding: ident ($encoding_fn:ident, $decoding_fn:ident)),+ $(,)?) => {
         $(
+            #[library_benchmark]
             fn $encoding_fn() {
-                let data = iai::black_box(bench_default());
+                let data = black_box(bench_default());
 
                 rasn::$encoding::encode(&data).unwrap();
             }
 
+            #[library_benchmark]
             fn $decoding_fn() {
                 use once_cell::sync::Lazy;
 
@@ -23,7 +28,9 @@ macro_rules! encoding_rules {
 
         )+
 
-        iai::main!{$($encoding_fn, $decoding_fn),+}
+        library_benchmark_group!(name = encode; benchmarks = $($encoding_fn),+);
+        library_benchmark_group!(name = decode; benchmarks = $($decoding_fn),+);
+        main!(library_benchmark_groups = encode, decode);
     }
 }
 
