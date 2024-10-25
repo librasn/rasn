a0ad8158137c2b7ac22ef001ed235b2f83700125
Niklas Saari <niklas.saari@tutanota.com>
2024-10-25 13:27:33 +0300

    fix: Don't override generics impl in proc-mac

--- macros/src/ext.rs
+++ macros/src/ext.rs
@@ -17,29 +17,26 @@ pub trait GenericsExt {
 impl GenericsExt for syn::Generics {
     fn add_trait_bounds(&mut self, crate_root: &syn::Path, ident: syn::Ident) {
         for param in self.type_params_mut() {
-            param.colon_token = Some(Default::default());
-            param.bounds = {
-                let mut punct = syn::punctuated::Punctuated::new();
-                punct.push(
-                    syn::TraitBound {
-                        paren_token: None,
-                        modifier: syn::TraitBoundModifier::None,
-                        lifetimes: None,
-                        path: {
-                            let mut path = crate_root.clone();
-                            path.segments.push(syn::PathSegment {
-                                ident: ident.clone(),
-                                arguments: syn::PathArguments::None,
-                            });
+            if param.colon_token.is_none() {
+                param.colon_token = Some(Default::default());
+            }
+            param.bounds.push(
+                syn::TraitBound {
+                    paren_token: None,
+                    modifier: syn::TraitBoundModifier::None,
+                    lifetimes: None,
+                    path: {
+                        let mut path = crate_root.clone();
+                        path.segments.push(syn::PathSegment {
+                            ident: ident.clone(),
+                            arguments: syn::PathArguments::None,
+                        });
 
-                            path
-                        },
-                    }
-                    .into(),
-                );
-
-                punct
-            };
+                        path
+                    },
+                }
+                .into(),
+            );
         }
     }
 }
