ee2faedbda169136208ebd01a4d2cbb4513961a4
Niklas Saari <niklas.saari@tutanota.com>
2024-10-25 22:45:35 +0300

    bench: run x509 benches only on x86_64 or aarch64

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -19,9 +19,10 @@ fn rasn(c: &mut Criterion) {
         }}
     }
 
-    bench_encoding_rules!(ber, der, cer, uper);
+    bench_encoding_rules!(ber, der, cer, uper, oer);
 }
 
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 fn x509(c: &mut Criterion) {
     let data: &[u8] = include_bytes!("../standards/pkix/tests/data/letsencrypt-x3.crt");
     let mut group = c.benchmark_group("X.509");
@@ -29,7 +30,13 @@ fn x509(c: &mut Criterion) {
         b.iter(|| black_box(rasn::der::decode::<rasn_pkix::Certificate>(data).unwrap()))
     });
     group.bench_function("x509-parser", |b| {
-        b.iter(|| black_box(<x509_parser::certificate::X509Certificate as x509_parser::prelude::FromDer<x509_parser::error::X509Error>>::from_der(data)))
+        b.iter(|| {
+            black_box(
+                <x509_parser::certificate::X509Certificate as x509_parser::prelude::FromDer<
+                    x509_parser::error::X509Error,
+                >>::from_der(data),
+            )
+        })
     });
     group.bench_function("x509-cert", |b| {
         b.iter(|| black_box(<x509_cert::Certificate as x509_cert::der::Decode>::from_der(data)))
@@ -40,5 +47,9 @@ fn x509(c: &mut Criterion) {
     group.finish();
 }
 
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 criterion_group!(codec, x509, rasn);
+
+#[cfg(not(any(target_arch = "x86_64", target_arch = "aarch64")))]
+criterion_group!(codec, rasn);
 criterion_main!(codec);
