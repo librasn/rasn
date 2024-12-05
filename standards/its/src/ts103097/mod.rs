d38326a0bae1c63a58150145e0008237f6a88b21
Niklas Saari <niklas.saari@tutanota.com>
2024-12-05 19:30:15 +0200

    Crate renaming

--- standards/its/src/ts103097/mod.rs
+++ standards/its/src/ts103097/mod.rs
@@ -1,4 +1,5 @@
+pub mod extension_module;
+
 use rasn::prelude::*;
-use rasn_etsi_ts103097_extensions::*;
 
 pub const ETSI_TS103097_MODULE_OID: &Oid = Oid::const_new(&[0, 4, 0, 5, 5, 103_097, 1, 3, 1]);
